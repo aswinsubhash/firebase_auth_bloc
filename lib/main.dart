086f9a5670907ed4839449b0df46adcb56f1364d
Aswin S <aswinofficial3@gmail.com>
2023-10-05 12:06:21 +0530

    Signin Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_bloc/blocs/auth/auth_bloc.dart';
+import 'package:firebase_auth_bloc/blocs/signin/signin_cubit.dart';
 import 'package:firebase_auth_bloc/repositories/auth_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -38,6 +39,11 @@ class MyApp extends StatelessWidget {
         providers: [
           BlocProvider<AuthBloc>(
             create: (context) => AuthBloc(
+              authRepository: context.read<AuthRepository>(),
+            ),
+          ),
+          BlocProvider<SigninCubit>(
+            create: (context) => SigninCubit(
               authRepository: context.read<AuthRepository>(),
             ),
           )
