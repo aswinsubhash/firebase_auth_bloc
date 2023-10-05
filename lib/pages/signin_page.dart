dfd65378912004f4024eb43ffdad6f4dd85f0775
Aswin S <aswinofficial3@gmail.com>
2023-10-05 12:24:42 +0530

    Signin

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_auth_bloc/pages/signup_page.dart';
+import 'package:firebase_auth_bloc/utils/error_dialogue.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:validators/validators.dart';
 
 import '../blocs/signin/signin_cubit.dart';
-
-
 
 class SigninPage extends StatefulWidget {
   static const String routeName = '/signin';
@@ -33,120 +32,124 @@ class _SigninPageState extends State<SigninPage> {
 
     print('email: $_email, password: $_password');
 
-  context.read<SigninCubit>().signin(email: _email!, password: _password!);
+    context.read<SigninCubit>().signin(email: _email!, password: _password!);
   }
 
   @override
   Widget build(BuildContext context) {
-   // final signinState = context.watch<SigninProvider>().state;
-
     return WillPopScope(
       onWillPop: () async => false,
       child: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child: Scaffold(
-          body: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: Form(
-                key: _formKey,
-                autovalidateMode: _autovalidateMode,
-                child: ListView(
-                  shrinkWrap: true,
-                  children: [
-                    Image.asset(
-                      'assets/images/flutter_logo.png',
-                      height: 250,
-                      width: 250,
-                    ),
-                    const SizedBox(height: 20.0),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      autocorrect: false,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        filled: true,
-                        labelText: 'Email',
-                        prefixIcon: Icon(Icons.email),
-                      ),
-                      validator: (String? value) {
-                        if (value == null || value.trim().isEmpty) {
-                          return 'Email required';
-                        }
-                        if (!isEmail(value.trim())) {
-                          return 'Enter a valid email';
-                        }
-                        return null;
-                      },
-                      onSaved: (String? value) {
-                        _email = value;
-                      },
-                    ),
-                    const SizedBox(height: 20.0),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        filled: true,
-                        labelText: 'Password',
-                        prefixIcon: Icon(Icons.lock),
-                      ),
-                      validator: (String? value) {
-                        if (value == null || value.trim().isEmpty) {
-                          return 'Password required';
-                        }
-                        if (value.trim().length < 6) {
-                          return 'Password must be at least 6 characters long';
-                        }
-                        return null;
-                      },
-                      onSaved: (String? value) {
-                        _password = value;
-                      },
-                    ),
-                    const SizedBox(height: 20.0),
-                    ElevatedButton(
-                      onPressed:
-                          // signinState.signinStatus == SigninStatus.submitting
-                          //     ? null
-                          //     :
-                               _submit,
-                      style: ElevatedButton.styleFrom(
-                        textStyle: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w600,
+        child: BlocConsumer<SigninCubit, SigninState>(
+          listener: (context, state) {
+            if (state.signinStatus == SigninStatus.error) {
+              errorDialog(context, state.customError);
+            }
+          },
+          builder: (context, state) {
+            return Scaffold(
+              body: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Form(
+                    key: _formKey,
+                    autovalidateMode: _autovalidateMode,
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Image.asset(
+                          'assets/images/flutter_logo.png',
+                          height: 250,
+                          width: 250,
                         ),
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                      ),
-                      child: Text(
-                          // signinState.signinStatus == SigninStatus.submitting
-                          //     ? 'Loading...'
-                          //     :
-                               'Sign In'),
-                    ),
-                    const SizedBox(height: 10.0),
-                    TextButton(
-                      onPressed: 
-                      // signinState.signinStatus ==
-                      //         SigninStatus.submitting
-                      //     ? null
-                      //     :
-                          () {
-                              Navigator.pushNamed(context, SignupPage.routeName);
-                            },
-                      style: TextButton.styleFrom(
-                        textStyle: const TextStyle(
-                          fontSize: 20.0,
-                          decoration: TextDecoration.underline,
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.email),
+                          ),
+                          validator: (String? value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Email required';
+                            }
+                            if (!isEmail(value.trim())) {
+                              return 'Enter a valid email';
+                            }
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            _email = value;
+                          },
                         ),
-                      ),
-                      child: const Text('Not a member? Sign up!'),
-                    )
-                  ],
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            labelText: 'Password',
+                            prefixIcon: Icon(Icons.lock),
+                          ),
+                          validator: (String? value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Password required';
+                            }
+                            if (value.trim().length < 6) {
+                              return 'Password must be at least 6 characters long';
+                            }
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            _password = value;
+                          },
+                        ),
+                        const SizedBox(height: 20.0),
+                        ElevatedButton(
+                          onPressed:
+                              state.signinStatus == SigninStatus.submitting
+                                  ? null
+                                  : _submit,
+                          style: ElevatedButton.styleFrom(
+                            textStyle: const TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          ),
+                          child: Text(
+                              state.signinStatus == SigninStatus.submitting
+                                  ? 'Loading...'
+                                  : 'Sign In'),
+                        ),
+                        const SizedBox(height: 10.0),
+                        TextButton(
+                          onPressed:
+                              state.signinStatus == SigninStatus.submitting
+                                  ? null
+                                  : () {
+                                      Navigator.pushNamed(
+                                          context, SignupPage.routeName);
+                                    },
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(
+                              fontSize: 20.0,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                          child: const Text('Not a member? Sign up!'),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
